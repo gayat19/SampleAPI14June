7f28947735442d0c784c29ced8f67c9cb80b8495
Gayathri Mahadevan <gayat_19@yahoo.com>
2021-06-14 16:14:13 +0530

    First-Commit

---
+++ SampleWebAPI/Migrations/20210614103910_Init-check-2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SampleWebAPI.Migrations
+{
+    public partial class Initcheck2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ SampleWebAPI/Models/CustomerManagementContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleWebAPI.Models
+{
+    public class CustomerManagementContext :DbContext
+    {
+        public CustomerManagementContext()
+        {
+
+        }
+        public CustomerManagementContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().HasData(new Customer
+            {
+                Id = 101,
+                Name = "Ramu",
+                Age = 21
+            });
+        }
+    }
+}
